eedad2f12c02e159b72559803ce175a4a19b2625
caoqiang <caoqianghappy@126.com>
2023-01-04 13:39:14 +0000

    up

--- libs/safe_eval.py
+++ libs/safe_eval.py
@@ -26,6 +26,8 @@ from types import CodeType
 
 from opcode import HAVE_ARGUMENT, opmap, opname
 
+import config
+
 from .log import Log
 
 __all__ = ['test_expr', 'safe_eval', 'const_eval']
@@ -294,7 +296,7 @@ def timeout(sec, raise_sec=1):
         return wrapped_func
     return decorator
 
-@timeout(3)
+@timeout(config.unsafe_eval_timeout)
 def unsafe_eval(*args, **kwargs) :
     return eval(*args, **kwargs)
 
@@ -375,7 +377,7 @@ def test_expr(expr, allowed_codes, mode="eval", filename=None):
     except (SyntaxError, TypeError, ValueError):
         raise
     except Exception as e:
-        raise ValueError('"%s" while compiling\n%r' % (ustr(e), expr))
+        raise ValueError('"%s" while compiling\n%r' % (e, expr))
     assert_valid_codeobj(allowed_codes, code_obj, expr)
     return code_obj
 
